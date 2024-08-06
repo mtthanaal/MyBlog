a9f46a362628c12a3f520929d28ef2f50f18f3f3
mtthanaal <fowkhan020605@gmail.com>
2024-08-06 11:42:07 +0530

    CSS for admin dashboard

--- MyBlog/resources/views/admin/adminhome.blade.php
+++ MyBlog/resources/views/admin/adminhome.blade.php
@@ -1,29 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head> 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Dark Bootstrap Admin </title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="robots" content="all,follow">
-    <!-- Bootstrap CSS-->
-    <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
-    <!-- Font Awesome CSS-->
-    <link rel="stylesheet" href="vendor/font-awesome/css/font-awesome.min.css">
-    <!-- Custom Font Icons CSS-->
-    <link rel="stylesheet" href="css/font.css">
-    <!-- Google fonts - Muli-->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Muli:300,400,700">
-    <!-- theme stylesheet-->
-    <link rel="stylesheet" href="css/style.default.css" id="theme-stylesheet">
-    <!-- Custom stylesheet - for your changes-->
-    <link rel="stylesheet" href="css/custom.css">
-    <!-- Favicon-->
-    <link rel="shortcut icon" href="img/favicon.ico">
-    <!-- Tweaks for older IEs--><!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
+    @include('admin.css')
   </head>
   <body>
     <header class="header">   
@@ -173,7 +151,7 @@
       <nav id="sidebar">
         <!-- Sidebar Header-->
         <div class="sidebar-header d-flex align-items-center">
-          <div class="avatar"><img src="img/avatar-6.jpg" alt="..." class="img-fluid rounded-circle"></div>
+          <div class="avatar"><img src="admincss/img/avatar-6.jpg" alt="..." class="img-fluid rounded-circle"></div>
           <div class="title">
             <h1 class="h5">Mark Stephen</h1>
             <p>Web Designer</p>
@@ -590,13 +568,13 @@
       </div>
     </div>
     <!-- JavaScript files-->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/popper.js/umd/popper.min.js"> </script>
-    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
-    <script src="vendor/jquery.cookie/jquery.cookie.js"> </script>
-    <script src="vendor/chart.js/Chart.min.js"></script>
-    <script src="vendor/jquery-validation/jquery.validate.min.js"></script>
-    <script src="js/charts-home.js"></script>
-    <script src="js/front.js"></script>
+    <script src="admincss/vendor/jquery/jquery.min.js"></script>
+    <script src="admincss/vendor/popper.js/umd/popper.min.js"> </script>
+    <script src="admincss/vendor/bootstrap/js/bootstrap.min.js"></script>
+    <script src="admincss/vendor/jquery.cookie/jquery.cookie.js"> </script>
+    <script src="admincss/vendor/chart.js/Chart.min.js"></script>
+    <script src="admincss/vendor/jquery-validation/jquery.validate.min.js"></script>
+    <script src="admincss/js/charts-home.js"></script>
+    <script src="admincss/js/front.js"></script>
   </body>
 </html>
