fffba5c7a67591010d09ba8cb212d819541bf878
mtthanaal <fowkhan020605@gmail.com>
2024-08-06 15:00:14 +0530

    Post page create and connect with database MySQL

--- MyBlog/app/Http/Controllers/AdminController.php
+++ MyBlog/app/Http/Controllers/AdminController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
@@ -10,5 +11,21 @@ class AdminController extends Controller
     public function post_page()
     {
         return view ('admin.post_page');
+    }
+    public function add_post(Request $request)
+    {
+        $post=new Post;
+        $post->title = $request->title;
+        $post->description = $request->description;
+
+        $image = $request->image;
+        $imagename=time().'.'.$image->getClientOriginalExtension();
+        $request->image->move('postimage',$imagename);
+        $post->image = $imagename;
+
+
+        $post->save();
+        return redirect()->back();
+
     }
 }
