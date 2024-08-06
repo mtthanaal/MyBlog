c5c447c2cedd764580af79ca1090f0c4b65e5864
mtthanaal <fowkhan020605@gmail.com>
2024-08-06 12:43:11 +0530

    Post page

---
+++ MyBlog/resources/views/admin/post_page.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+  <head> 
+    @include('admin.css')
+    <style type="text/css">
+
+        .post_title
+        {
+          font-size: 30px;
+          font-weight: bold;
+          text-align: center;
+          padding: 30px;
+
+        }
+
+    </style>    
+  </head>
+  <body>
+
+
+        @include('admin.header')
+    
+
+    <div class="d-flex align-items-stretch">
+      <!-- Sidebar Navigation-->
+
+        @include('admin.sidebar')
+
+      <!-- Sidebar Navigation end-->
+      
+    <div class="page-content">
+
+        <h1 class="post_title">Add Post</h1>
+        
+
+    </div>
+
+        @include('admin.footer')
+  </body>
+</html>
