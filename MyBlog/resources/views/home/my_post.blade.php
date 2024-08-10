eeba72ee1e25342e23841a06a7ee3c020edd26d7
mtthanaal <fowkhan020605@gmail.com>
2024-08-10 15:56:10 +0530

    Add ckeditor and table for user page

--- MyBlog/resources/views/home/my_post.blade.php
+++ MyBlog/resources/views/home/my_post.blade.php
@@ -5,40 +5,35 @@
       @include('home.homecss')
 
       <style type="text/css">
-         
-        .post_deg
-        {
+         .table_container {
             padding: 30px;
-            text-align: center;
-            background-color:black ;
-        }
-        .title_deg
-        {
-            font-size: 30px;
-            font-weight:bold ;
-            padding: 15px;
+            background-color: ;
             color: white;
-        }
-        .des_deg
-        {
-            font-size: 18px;
-            font-weight:bold ;
-            padding: 15px;
-            color: whitesmoke;
-        }
-        .img_deg
-        {
-            height: 200px;
-            width: 300px;
-            padding: 30px;
             margin: auto;
-        }
-
-
+         }
+         .table_deg {
+            width: 100%;
+            border-collapse: collapse;
+         }
+         .table_deg th, .table_deg td {
+            border: 1px solid white;
+            padding: 10px;
+            text-align: center;
+         }
+         .table_deg th {
+            font-size: 20px;
+            font-weight: bold;
+         }
+         .table_deg td {
+            font-size: 16px;
+         }
+         
+         .action_btns {
+            display: flex;
+            justify-content: center;
+            gap: 10px;
+         }
       </style>
-
-
-
    </head>
    <body>
       <!-- header section start -->
@@ -46,41 +41,37 @@
          @include('home.header')
 
          @if (session()->has('message'))
-
          <div class="alert alert-success">
-
             <button type="button" class="close" data-dismiss="alert" aria-hidden="true">X</button>
-
             {{session()->get('message')}}
-
-
          </div>
-
-
-         
          @endif
 
-         @foreach ( $data as $data )
-         
-         
-
-         <div class="post_deg">
-            <img class="img_deg" src="/postimage/{{$data->image}}">
-            <h4 class="title_deg">{{$data->title}}</h4>
-            <p class="des_deg">{{$data->description}}</p>
-
-            <a onclick="return confirm('Are you sure to delete this?')" href="{{url('my_post_del',$data->id)}}" class="btn btn-danger">Delete</a>
-
-            <a href="{{url('post_update_page',$data->id)}}" class="btn btn-primary">Update</a>
-
-
-
-
+         <div class="table_container">
+            <table class="table_deg">
+               <thead>
+                  <tr>
+                     <th>Image</th>
+                     <th>Title</th>
+                     <th>Description</th>
+                     <th>Actions</th>
+                  </tr>
+               </thead>
+               <tbody>
+                  @foreach ( $data as $data )
+                  <tr>
+                     <td><img class="img_deg" src="/postimage/{{$data->image}}" alt="{{$data->title}}"></td>
+                     <td>{{$data->title}}</td>
+                     <td>{{$data->description}}</td>
+                     <td class="action_btns">
+                        <a onclick="return confirm('Are you sure to delete this?')" href="{{url('my_post_del',$data->id)}}" class="btn btn-danger">Delete</a>
+                        <a href="{{url('post_update_page',$data->id)}}" class="btn btn-primary">Update</a>
+                     </td>
+                  </tr>
+                  @endforeach
+               </tbody>
+            </table>
          </div>
-
-         @endforeach
-
-         
 
       <!-- footer section start -->
       @include('home.footer')
@@ -100,11 +91,9 @@
       <script src="js/bootstrap.bundle.min.js"></script>
       <script src="js/jquery-3.0.0.min.js"></script>
       <script src="js/plugin.js"></script>
-
       <!-- sidebar -->
       <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
       <script src="js/custom.js"></script>
-      
       <!-- javascript --> 
       <script src="js/owl.carousel.js"></script>
       <script src="https:cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>    
