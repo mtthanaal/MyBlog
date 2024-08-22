a564e2099e9bbc790e6e3c5e520c1bd116516d3b
mtthanaal <fowkhan020605@gmail.com>
2024-08-22 15:58:53 +0530

    CSS for blog post view

--- MyBlog/resources/views/home/about.blade.php
+++ MyBlog/resources/views/home/about.blade.php
@@ -1,24 +1,29 @@
 <div class="about_section layout_padding">
-         <div class="container-fluid">
-            <div class="row">
-               <div class="col-md-6">
-                  <div class="about_taital_main">
-                     <h1 class="about_taital">About Our Page</h1>
-                     <p class="about_text">Sri Lanka, historically known as Ceylon, and officially the Democratic Socialist Republic of Sri Lanka, is an island country in South Asia. It lies in the Indian Ocean, southwest of the Bay of Bengal, separated from the Indian peninsula by the Gulf of Mannar and the Palk Strait. </p>
-                       <h3>Capitals: Colombo, Sri Jayawardenepura Kotte</h3> 
-                        <h3>Currency: Sri Lankan Rupee</h3>
-                        <h3>Official languages: Sinhala, Tamil</h3>
-                        <h3>President: Ranil Wickremesinghe</h3>
-                        <h3>Prime minister: Dinesh Gunawardena</h3>
-                        <h3>Government: Unitary state, Constitutional republic, Semi-presidential system</h3>
-                        <h3>Population: 22.18 million (2022) World Bank </h3>
-                     <div class="readmore_bt"><a href="https://en.wikipedia.org/wiki/Sri_Lanka">Read More</a></div>
-                  </div>
-               </div>
-               <div class="col-md-6 padding_right_0">
-                  <div>
-                     <img src="images/about-img.png" class="about_img" style="height: 700px; width: 600px;"></div>
-                  </div>
+   <div class="container-fluid">
+      <div class="row">
+         <div class="col-md-6">
+            <div class="about_taital_main">
+               <h1 class="about_taital">About Our Page</h1>
+               <p class="about_text" style=" margin-bottom: 20px;">
+                  Sri Lanka, historically known as Ceylon, and officially the Democratic Socialist Republic of Sri Lanka, 
+                  is an island country in South Asia. It lies in the Indian Ocean, southwest of the Bay of Bengal, 
+                  separated from the Indian peninsula by the Gulf of Mannar and the Palk Strait.
+               </p>
+               <h4 style="margin-top: 20px;">Capitals: Colombo, Sri Jayawardenepura Kotte</h4> 
+               <h4>Currency: Sri Lankan Rupee</h4>
+               <h4>Official languages: Sinhala, Tamil</h4>
+               <h4>President: Ranil Wickremesinghe</h4>
+               <h4>Prime minister: Dinesh Gunawardena</h4>
+               <h4>Government: Unitary state, Constitutional republic, Semi-presidential system</h4>
+               <h4>Population: 22.18 million (2022) World Bank </h4>
+               <div class="readmore_bt"><a href="https://en.wikipedia.org/wiki/Sri_Lanka">Read More</a></div>
+            </div>
+         </div>
+         <div class="col-md-6 padding_right_0">
+            <div>
+               <img src="images/about-img.png" class="about_img" style="height: 700px; width: 600px;">
             </div>
          </div>
       </div>
+   </div>
+</div>
--- MyBlog/resources/views/home/services.blade.php
+++ MyBlog/resources/views/home/services.blade.php
@@ -1,18 +1,28 @@
 <div class="services_section layout_padding">
-         <div class="container">
-            <h1 class="services_taital">Blog Posts </h1>
-            <p class="services_text">Outdoor Activities in Sri Lanka</p>
-            <div class="services_section_2">
-               <div class="row">
-                  @foreach ( $post as $post )
-                  <div class="col-md-4" style="padding: 30px;">
-                     <div><img style="margin: bottom 20px; height: 200px" width="350px" src="/postimage/{{$post->image}}" class="services_img"></div>
-                     <h4>{{$post->title}}</h4>
-                     <p>Post by <b>{{$post->name}}</b> </p>
-                     <div class="btn_main"><a href="{{url('post_details',$post->id)}}">Read More</a></div>
-                  </div>
-                  @endforeach   
-               </div>
+    <div class="container">
+        <h1 class="services_taital">Blog Posts</h1>
+        <p class="services_text">Visit Sri Lanka Here</p>
+        <div class="services_section_2">
+            <div class="row">
+                @foreach ($post as $post)
+                <div class="col-md-4" style="padding: 30px;">
+                    <div class="image-container" style="position: relative; height: 200px; width: 350px; margin-bottom: 20px; overflow: hidden;">
+                        <img src="/postimage/{{$post->image}}" class="services_img" style="height: 100%; width: 100%; object-fit: cover;">
+                        <h4 class="post-title" style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); color: white; text-shadow: 2px 2px 6px rgba(0, 0, 0, 0.7); background-color: rgba(0, 0, 0, 0.5); padding: 10px 20px; border-radius: 5px; transition: opacity 0.3s ease;">
+                            {{$post->title}}
+                        </h4>
+                    </div>
+                    <p class="btn_main">Post by <b>{{$post->name}}</b></p>
+                    <div class="btn_main"><a href="{{url('post_details', $post->id)}}">Read More</a></div>
+                </div>
+                @endforeach
             </div>
-         </div>
+        </div>
+    </div>
 </div>
+
+<style>
+    .image-container:hover .post-title {
+        opacity: 0;
+    }
+</style>
